0a91c551289434e82c51050664674055cc36d043
bookq_surface <bookq@outlook.com>
2022-07-15 20:10:00 +0800

    partly fix concurrent problem when closing event

--- bot/longEvent.go
+++ bot/longEvent.go
@@ -14,26 +14,26 @@ const (
 )
 
 func longEventImageInsert(e Pichubot.LongEvent, rs int32) (map[string]interface{}, bool) {
-	defer e.Close()
 	for {
 		msg := <-*e.Channel
 		if msg == "取消" {
 			return nil, true
 		}
 		if cqcode.CQImage.All.FindIndex([]byte(msg)) != nil {
+			e.Close() // 很可能关闭channel的时候要拿锁，而这个框架没拿，导致了关了channel但没删事件的时候访问了close的channel然后panic了
 			return imagestore.InsertImageFromMessage(msg, rs), false
 		}
 	}
 }
 
 func longEventImageUpdate(e Pichubot.LongEvent, priv int32) error {
-	defer e.Close()
 	for {
 		msg := <-*e.Channel
 		if msg == "取消" {
 			return errors.New("取消了添加")
 		}
 		if cqcode.CQImage.All.FindIndex([]byte(msg)) != nil {
+			e.Close() // 同上
 			return imagestore.UpdateOneFromMessage(msg, priv)
 		}
 	}
